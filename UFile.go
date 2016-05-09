dedf3d46e216af772a88512319e4101ad17f828c
himyxia <aiwei18@gmail.com>
2016-05-09 17:38:39 +0800

    correct retry number

--- UFile.go
+++ UFile.go
@@ -103,9 +103,10 @@ func (self *UcloudApiClient) PutFile(fileName, bucketName, contentType string, d
 	if err != nil {
 		return err
 	}
+	ori := retryNum
 	if resp.StatusCode != http.StatusOK {
 		if retryNum == 0 {
-			return fmt.Errorf("Internal Server Error, retry: %v, ucloud resp: %+v", retryNum, resp)
+			return fmt.Errorf("Internal Server Error, retry: %v, ucloud resp: %+v", ori+1, resp)
 		}
 		time.Sleep(time.Second * 1)
 		retryNum--
